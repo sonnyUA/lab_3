f0c6653fca8ea42a612ced6bca3d317d716d539e
sonnyUA <58081731+sonnyUA@users.noreply.github.com>
2019-12-08 16:25:52 +0200

    Add files via upload

---
+++ Frontend Test/main.js
@@ -0,0 +1,14 @@
+function openTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("nav__item-tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+    evt.currentTarget.classList.add("active");
+}
